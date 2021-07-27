286ba62926462ffff31fa19518eaf195bef05742
zhongchubing <zhongchubing@runstech.com>
2021-07-27 14:05:46 +0800

    feat: ant-design-pro5项目

---
+++ ant-design-pro5/src/pages/Welcome.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Card, Alert, Typography } from 'antd';
+import styles from './Welcome.less';
+import ProTable from '@ant-design/pro-table';
+
+const CodePreview: React.FC = ({ children }) => (
+  <pre className={styles.pre}>
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+
+export default (): React.ReactNode => {
+  return (
+    <PageContainer>
+      <Card>
+        <Alert
+          message={'更快更强的重型组件，已经发布1。'}
+          type="success"
+          showIcon
+          banner
+          style={{
+            margin: -12,
+            marginBottom: 24,
+          }}
+        />
+        <Typography.Text strong>
+          高级表格{' '}
+          <a
+            href="https://procomponents.ant.design/components/table"
+            rel="noopener noreferrer"
+            target="__blank"
+          >
+            欢迎使用
+          </a>
+        </Typography.Text>
+        <CodePreview>yarn add @ant-design/pro-table</CodePreview>
+        <Typography.Text
+          strong
+          style={{
+            marginBottom: 12,
+          }}
+        >
+          高级布局{' '}
+          <a
+            href="https://procomponents.ant.design/components/layout"
+            rel="noopener noreferrer"
+            target="__blank"
+          >
+            欢迎使用
+          </a>
+        </Typography.Text>
+        <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
+
+        <ProTable columns={[{key: 'name', title: '名称'}, {key: 'name', title: '名称2'}, {key: 'name', title: '名称3'}]} />
+      </Card>
+    </PageContainer>
+  );
+};
